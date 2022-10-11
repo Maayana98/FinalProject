0f205b4f8db321479ad8aa940a24c9741c4ddd6f
Nir Karo <nyk432@gmail.com>
2022-10-11 13:20:53 +0300

    building models

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const mongoose = require("mongoose");
+
+const connectToDB = async () => {
+    await mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+}
+
+connectToDB();
 
 app.get('/' , (req,res) => {
-    res.send('This is a test print');
+    res.send('This is a test'); //test printing..
 });
 
 app.listen(process.env.PORT , () => {
